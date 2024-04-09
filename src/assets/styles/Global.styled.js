9be62b99b9963f6e33abd0a437b597f9ed8b2897
kidboi666 <kidboi666@gmail.com>
2024-04-09 23:12:06 +0900

    chore : reset css 스타일 추가, color 변수 추가

--- src/assets/styles/Global.styled.js
+++ src/assets/styles/Global.styled.js
@@ -67,7 +67,43 @@ a {
 /* --------- color constants --------- */
 
 :root {
-  /* ---- */
+  --error-color: #DC3A3A;
+  --black-color: #000000;
+  --white-color: #ffffff;
+  --surface-color: #F6F8FF;
+  --purple-100: #F8F0FF;
+  --purple-200: #ECD9FF;
+  --purple-300: #DCB9FF;
+  --purple-400: #C894FD;
+  --purple-500: #AB57FF;
+  --purple-600: #9935FF;
+  --purple-700: #861DEE;
+  --purple-800: #6E0AD1;
+  --purple-900: #5603A7;
+  --orange-100: #FFF0D6;
+  --orange-200: #FFE2AD;
+  --orange-300: #FFC583;
+  --orange-400: #FFAE65;
+  --orange-500: #FF8832;
+  --blue-100: #E2F5FF;
+  --blue-200: #B1E4FF;
+  --blue-300: #7CD2FF;
+  --blue-400: #34B9FF;
+  --blue-500: #00A2FE;
+  --green-100: #E4FBDC;
+  --green-200: #D0F5C3;
+  --green-300: #9BE282;
+  --green-400: #60CF37;
+  --green-500: #2BA600;
+  --grayscale-100: #F6F6F6;
+  --grayscale-200: #EEEEEE;
+  --grayscale-300: #CCCCCC;
+  --grayscale-400: #999999;
+  --grayscale-500: #555555;
+  --grayscale-600: #4A4A4A;
+  --grayscale-700: #3A3A3A;
+  --grayscale-800: #2B2B2B;
+  --grayscale-900: #181818;
 }
 
 `;
