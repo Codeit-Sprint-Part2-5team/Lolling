2cc6180cee3a99e7a073408ae995523de692ab64
kidboi666 <kidboi666@gmail.com>
2024-04-10 20:06:04 +0900

    feat : 롤링 페이퍼 생성 api 요청 구현

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,12 +1,30 @@
 import axios from 'axios';
 
 const API_URL = `https://rolling-api.vercel.app`;
+const TEAM_URL = `https://rolling-api.vercel.app/5-5/recipients`;
 
 export const getMockImageRequest = async () => {
   const response = await axios.get(`${API_URL}/profile-images/`);
 
   if (response.status < 200 || response.status >= 300) {
     throw new Error('프로필 이미지 가져오기 실패');
+  }
+
+  return response;
+};
+
+export const createCardFolderRequest = async (userName, backgroundColor) => {
+  const response = await axios({
+    method: 'post',
+    url: `${TEAM_URL}/`,
+    data: {
+      name: userName,
+      backgroundColor: backgroundColor,
+    },
+  });
+
+  if (response.status < 200 || response.status >= 300) {
+    throw new Error('롤링 페이퍼 생성 실패');
   }
 
   return response;
