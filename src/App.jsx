be20ca670d3845af8069a51a61150cb773af7d73
kidboi666 <kidboi666@gmail.com>
2024-04-10 18:01:36 +0900

    fix : App.jsx 에 임시로 출력해놓은 컴포넌트 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from './assets/styles/Global.styled';
-import CardFolder from './components/CardFolder/CardFolder';
 import Header from './components/Header/Header';
 function App() {
   return (
     <>
       <GlobalStyle />
-      <CardFolder />
       <Header />
       <Outlet />
     </>
