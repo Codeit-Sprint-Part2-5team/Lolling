0641754c9a3295e85139a8243fbaf3c538db7eec
O-daeun <qzsz797979@gmail.com>
2024-04-10 14:57:16 +0900

    Docs : Header 컴포넌트 생성 및 App.jsx에 추가 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import GlobalStyle from "./assets/styles/Global.styled";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Outlet />
     </>
   );
