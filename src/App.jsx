5d73f5a43635aa8fa23be820a43aa1887b4fe094
O-daeun <qzsz797979@gmail.com>
2024-04-10 13:25:52 +0900

    feat : Card 컴포넌트 기본 작업 및 스타일 적용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Card from "./components/Card/Card";
 
 function App() {
-  return (
-    <>
-      <Card />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -1,9 +1,59 @@
 import styled from "styled-components";
 
 export const CardLayout = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 384px;
   height: 280px;
+  padding: 28px 24px 24px;
   border-radius: 16px;
   box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
-  background-color: #fff;
+  background-color: var(--white);
+`;
+
+export const TopContainer = styled.div`
+  display: flex;
+  gap: 14px;
+  padding-bottom: 15px;
+  border-bottom: 1px solid var(--gray-200);
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const NameContainer = styled.div`
+  display: flex;
+  gap: 6px;
+  font-size: 20px;
+`;
+
+export const BottomContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  padding-top: 16px;
+`;
+
+export const ContentBox = styled.p`
+  display: -webkit-box;
+  width: 100%;
+  overflow: hidden;
+  font-size: 18px;
+  line-height: 28px;
+  color: var(--gray-600);
+  letter-spacing: -0.18px;
+  text-overflow: ellipsis;
+  word-break: break-word;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`;
+
+export const DateBox = styled.p`
+  font-size: 12px;
+  color: var(--gray-400);
+  letter-spacing: -0.06px;
 `;
