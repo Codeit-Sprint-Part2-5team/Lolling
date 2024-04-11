16bfe1bf09db58be416fbedccae7786e2782ba51
eqypo9 <eqypo@naver.com>
2024-04-11 16:05:04 +0900

    feat: useState로 input의 상태 변경하도록 수정

--- src/components/TextField/Input/Input.jsx
+++ src/components/TextField/Input/Input.jsx
@@ -1,12 +1,38 @@
+import React, { useState } from 'react';
 import * as S from './Input.styled';
 
-function Input({ active, error, submitted, ...props }) {
+function Input({ ...props }) {
+  const [active, setActive] = useState(false);
   const [focused, setFocused] = useState(false);
   const [clickedOutside, setClickedOutside] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleClickOutside = () => {
     setClickedOutside(true);
     setFocused(false);
+  };
+
+  const handleFocus = () => {
+    setActive(true);
+    setFocused(true);
+    setClickedOutside(false);
+  };
+
+  const handleBlur = () => {
+    setActive(false);
+    setFocused(false);
+    setClickedOutside(false);
+  };
+
+  const handleSubmission = () => {
+    // 어떤 조건에서 에러 발생한다고 가정 -> 추후 수정
+    if (props.value === '') {
+      setError(true);
+    } else {
+      setError(false);
+      setSubmitted(true);
+    }
   };
 
   return (
@@ -25,11 +51,12 @@ function Input({ active, error, submitted, ...props }) {
             ? 'disabled'
             : ''
         }
-        onBlur={handleClickOutside}
-        onFocus={() => setFocused(true)}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
         {...props}
       />
       {error && <S.ErrorMessage>Error Message</S.ErrorMessage>}
+      <button onClick={handleSubmission}>Submit</button>
     </S.InputLayout>
   );
 }
