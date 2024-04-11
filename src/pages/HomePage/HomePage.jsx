078738fb1ef4d6a5a6dcb9a43c685c5df6863add
JunYoung <wnsdud96330@gmail.com>
2024-04-11 13:16:11 +0900

    design : 홈페이지 되돌리기

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,5 @@
 import React from 'react';
-import DeleteButton from '../../components/DeleteButton/DeleteButton';
-import ArrowButton from '../../components/ArrowButton/ArrowButton';
-import ToggleButton from '../../components/ToggleButton/ToggleButton';
-import PlusButton from '../../components/PlusButton/PlusButton';
 
 export default function HomePage() {
-  return (
-    <>
-      <div>HomePage</div>
-      <DeleteButton />
-      <ArrowButton arrowDirection={'left'} />
-      <ArrowButton arrowDirection={'right'} />
-      <ToggleButton />
-      <PlusButton />
-    </>
-  );
+  return <div>HomePage</div>;
 }
