eac20ce3f8fa472a06277cce8f2d7005d5ae833e
kidboi666 <kidboi666@gmail.com>
2024-04-11 22:11:13 +0900

    feat : post page html 태그 작성

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -5,7 +5,25 @@ import * as S from './PostPage.styled';
 export default function PostPage() {
   return (
     <S.PostPageLayout>
-      <Inner>PostPage</Inner>
+      <Inner>
+        <div>
+          <form>
+            <h4>To.</h4>
+            <input placeholder='받는 사람 이름을 입력해 주세요' />
+            <h4>배경화면을 선택해 주세요.</h4>
+            <p>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
+            <button>컬러</button>
+            <button>이미지</button>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <button>생성하기</button>
+          </form>
+        </div>
+      </Inner>
     </S.PostPageLayout>
   );
 }
