2caa82b19fec94ce06036e73d2455ad5ba99f7ec
이보미 <qhal6550@naver.com>
2024-04-11 17:54:13 +0900

    fit : 뱃지 컴포넌트 오류 수정

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,32 +1,9 @@
-import { useState } from 'react';
 import * as S from './Badge.styled';
 
 function Badge({ name }) {
-  const [backgroundColor, setBackgroundColor] = useState(--orange - 100);
-  const [color, setColor] = useState(--orange - 100);
-
-  switch (name) {
-    case '지인':
-      setBackgroundColor = '--orange-100,#FFF0D6';
-      setColor = '--orange-500,#FF8832';
-      break;
-    case '동료':
-      setBackgroundColor = '--purple-100,#F8F0FF';
-      setColor = '--purple-600,#9935FF';
-      break;
-    case '가족':
-      setBackgroundColor = '--green-,#E4FBDC';
-      setColor = '--green-500,#2BA600';
-      break;
-    case '친구':
-    default:
-      setBackgroundColor = '--blue-100,#E2F5FF';
-      setColor = '--blue-500,#00A2FE';
-  }
-
   return (
-    <S.BadgeLayout setBackgroundColor={setBackgroundColor}>
-      <S.BadgeTextBox setColor={setColor}>{name}</S.BadgeTextBox>
+    <S.BadgeLayout name={name}>
+      <S.BadgeTextBox name={name}>{name}</S.BadgeTextBox>
     </S.BadgeLayout>
   );
 }
