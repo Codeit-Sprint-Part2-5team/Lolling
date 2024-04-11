4c2390b0ce69aab9d472a4c2590fab160673b011
kidboi666 <kidboi666@gmail.com>
2024-04-11 23:39:06 +0900

    design : post 페이지 css 틀만 잡음

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
+import { useState } from 'react';
 import Inner from '../../components/Inner/Inner';
 import * as S from './PostPage.styled';
+import useAsync from '../../hooks/useAsync';
+import { createCardFolderRequest } from '../../apis/api';
+
+const INIT_CREATE_ROLL_PAPER = {
+  userName: '',
+  backgroundColor: '',
+};
 
 export default function PostPage() {
+  const [rollPaperBody, setRollPaperBody] = useState(INIT_CREATE_ROLL_PAPER);
+  const { requestFunction: createRequest } = useAsync(createCardFolderRequest);
+
+  const onChangeInputHandler = (e) => {
+    setRollPaperBody({
+      ...rollPaperBody,
+      [e.target['name']]: e.target.value,
+    });
+  };
+
+  const createPaper = async (e) => {
+    e.preventDefault();
+    const result = await createRequest(rollPaperBody);
+    if (!result) return;
+
+    setRollPaperBody(INIT_CREATE_ROLL_PAPER);
+  };
+
   return (
     <S.PostPageLayout>
       <Inner>
-        <div>
-          <form>
-            <h4>To.</h4>
-            <input placeholder='받는 사람 이름을 입력해 주세요' />
-            <h4>배경화면을 선택해 주세요.</h4>
-            <p>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
-            <button>컬러</button>
-            <button>이미지</button>
-            <div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <button>생성하기</button>
-          </form>
-        </div>
+        <S.FormContainer onSubmit={createPaper}>
+          <h4>To.</h4>
+          <input
+            name='userName'
+            value={rollPaperBody.userName}
+            onChange={onChangeInputHandler}
+            placeholder='받는 사람 이름을 입력해 주세요'
+          />
+          <h4>배경화면을 선택해 주세요.</h4>
+          <p>컬러를 선택하거나 이미지를 선택할 수 있습니다.</p>
+          <S.SelectingContainer>
+            <button type='button'>컬러</button>
+            <button type='button'>이미지</button>
+          </S.SelectingContainer>
+          <select
+            name='backgroundColor'
+            value={rollPaperBody.backgroundColor}
+            onChange={onChangeInputHandler}
+          >
+            <option value='beige'>주황</option>
+            <option value='purple'>보라</option>
+            <option value='blue'>파랑</option>
+            <option value='green'>초록</option>
+          </select>
+          <S.Button>생성하기</S.Button>
+        </S.FormContainer>
       </Inner>
     </S.PostPageLayout>
   );
--- src/pages/PostPage/PostPage.styled.js
+++ src/pages/PostPage/PostPage.styled.js
@@ -1,5 +1,63 @@
 import styled from 'styled-components';
 
-export const PostPageLayout = styled.div`
+export const PostPageLayout = styled.div``;
 
+export const FormContainer = styled.div`
+  > h4 {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  > p {
+    color: var(--gray-500, #555555);
+  }
+
+  > input {
+    width: 100%;
+    padding: 12px 16px;
+    border: 1px solid var(--gray-300, #cccccc);
+    border-radius: 8px;
+  }
+`;
+
+export const SelectingContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 40px;
+
+  > button {
+    width: 122px;
+    padding: 8px 16px;
+    cursor: pointer;
+    background-color: var(--gray-100, #f6f6f6);
+    &:first-child {
+      border-radius: 6px 0 0 6px;
+    }
+    &:last-child {
+      border-radius: 0 6px 6px 0;
+    }
+
+    &:hover,
+    &:active {
+      border: 2px solid var(--purple-600);
+      border-radius: 6px;
+      background-color: var(--white-color);
+      color: var(--purple-700);
+      font-weight: 700;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  display: block;
+  width: 100%;
+  padding: 15px 0;
+  border-radius: 10px;
+  background-color: var(--purple-600, #9935ff);
+  color: var(--white-color);
+  cursor: pointer;
+
+  &:active {
+    background-color: var(--purple-600);
+  }
 `;
