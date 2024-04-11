7db759f225bdab00ca557a88e956da2ed06dfc1d
JunYoung <wnsdud96330@gmail.com>
2024-04-12 00:37:30 +0900

    fix : 홈페이지 버튼 삭제

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import Button from '../../components/Button/Button';
 
 export default function HomePage() {
-  return (
-    <div>
-      HomePage
-      <Button text={'text'} variant={'primary'} size={56} width={500} />
-    </div>
-  );
+  return <div>HomePage</div>;
 }
