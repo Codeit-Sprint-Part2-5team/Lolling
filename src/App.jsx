3fa4f14db435993aa53831c260ab62abcb1a1f0e
O-daeun <qzsz797979@gmail.com>
2024-04-11 13:03:43 +0900

    docs : 테스트 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import GlobalStyle from "./assets/styles/Global.styled";
 import Header from "./components/Header/Header";
-import Card from "./components/Card/Card";
 function App() {
   return (
     <>
