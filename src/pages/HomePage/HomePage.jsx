3eb79b67f1402105ee04f83b6ccc5efcdb0fa54d
이보미 <qhal6550@naver.com>
2024-04-12 18:42:36 +0900

    design : 홈페이지 소개 부분 일부 완성

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,23 @@ import * as S from './HomePage.styled';
 export default function HomePage() {
   return (
     <S.HomePageLayout>
-      <Inner>HomePage</Inner>
+      <Inner>
+        <S.MainContainer>
+          <S.FirstContainer>
+            <S.FirstContentContainer>
+              <S.PointBox>
+                <S.PointContentBox>Point. 01 </S.PointContentBox>
+              </S.PointBox>
+              <S.Content1>
+                누구나 손쉽게, 온라인
+                <br /> 롤링 페이퍼를 만들 수 있어요
+              </S.Content1>
+              <S.Content2>로그인 없이 자유롭게 만들어요.</S.Content2>
+            </S.FirstContentContainer>
+            <S.CardImageBox></S.CardImageBox>
+          </S.FirstContainer>
+        </S.MainContainer>
+      </Inner>
     </S.HomePageLayout>
   );
 }
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,5 +1,83 @@
 import styled from 'styled-components';
+import { ReactComponent as CardImageSvg } from '../../assets/images/card.svg';
 
 export const HomePageLayout = styled.div`
+  width: 1920px;
+  height: 1080px;
+  background: #fff;
+`;
 
+export const MainContainer = styled.div`
+  display: flex;
+  width: 1200px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 30px;
+`;
+
+export const FirstContainer = styled.div`
+  display: flex;
+  padding: 60px 0px 60px 60px;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 152px;
+  border-radius: 16px;
+  background: var(--Surface, #f6f8ff);
+`;
+
+export const FirstContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const PointBox = styled.div`
+  display: flex;
+  padding: 6px 12px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  background: var(--Purple-600, #9935ff);
+`;
+
+export const PointContentBox = styled.p`
+  color: var(--white, #fff);
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: -0.07px;
+`;
+
+export const Content1 = styled.p`
+  color: var(--gray-900, #181818);
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 36px;
+  letter-spacing: -0.24px;
+  margin-top: 16px;
+  white-space: nowrap;
+`;
+
+export const Content2 = styled.p`
+  color: var(--gray-500, #555);
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px;
+  letter-spacing: -0.18px;
+  margin-top: 8px;
+`;
+
+export const CardImageBox = styled(CardImageSvg)`
+  display: flex;
+  width: 720px;
+  height: 204px;
+  justify-content: center;
+  align-items: center;
 `;
