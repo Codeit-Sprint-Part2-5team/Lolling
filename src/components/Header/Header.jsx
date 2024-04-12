1767fd42f612f490ed9e1ea451f451abc89b8fae
O-daeun <qzsz797979@gmail.com>
2024-04-12 10:23:06 +0900

    feat : Header 컴포넌트 생성 및 스타일링 완료

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React from 'react';
+import Inner from '../Inner/Inner';
+import Logo from '../../assets/images/logo.svg';
+import * as S from './Header.styled';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <S.HeaderLayout>
+      <Inner>
+        <S.LogoBox>
+          <S.LinkBox to='/'>
+            <img src={Logo} alt='Rolling logo' />
+            Rolling
+          </S.LinkBox>
+        </S.LogoBox>
+      </Inner>
+    </S.HeaderLayout>
+  );
 }
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const HeaderLayout = styled.header``;
+
+export const LogoBox = styled.h1``;
+
+export const LinkBox = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 19px;
+  font-weight: 700;
+  color: #4A494F;
+`;
