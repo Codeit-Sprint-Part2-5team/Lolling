8e722b44ef2089ff7e014fb02e11c9ea7ea85491
kidboi666 <kidboi666@gmail.com>
2024-04-12 16:02:43 +0900

    remove : App.jsx 에 불필요한 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from './assets/styles/Global.styled';
 import Header from './components/Header/Header';
-import ColorOption from './components/ColorOption/ColorOption';
-import CardFolder from './components/CardFolder/CardFolder';
 
 function App() {
   return (
