fd68d47b623ba595c2a34b7b25807b84a5eb88fb
kidboi666 <kidboi666@gmail.com>
2024-04-12 19:30:54 +0900

    refactor : App.jsx 불필요한 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from './assets/styles/Global.styled';
 import Header from './components/Header/Header';
-import CardFolder from './components/CardFolder/CardFolder';
 
 function App() {
   return (
