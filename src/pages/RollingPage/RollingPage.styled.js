699c970d3cd70f6eb7059dcfb2b84b76cd4ccaab
O-daeun <qzsz797979@gmail.com>
2024-04-12 11:16:00 +0900

    style : RollingPage에 Card 컴포넌트 임의로 정렬하여 스타일 작업 완료

--- src/pages/RollingPage/RollingPage.styled.js
+++ src/pages/RollingPage/RollingPage.styled.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 export const RollingPageLayout = styled.div`
+  padding: 113px 0 246px;
   background: var(--Orange-200, #FFE2AD);
 `; 
 
 
 export const CardContainer = styled.ul`
   display: flex;
-  gap: 24px;
+  gap: 28px 24px;
   flex-wrap: wrap;
 `
