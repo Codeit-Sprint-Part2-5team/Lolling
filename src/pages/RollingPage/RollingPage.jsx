bd4ef3283cf89abc19688bea94a0e9cb8e13012e
O-daeun <qzsz797979@gmail.com>
2024-04-12 14:36:21 +0900

    feat : RollingPage api 연결 및 Card에 props 전달 완료

--- src/pages/RollingPage/RollingPage.jsx
+++ src/pages/RollingPage/RollingPage.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Inner from '../../components/Inner/Inner';
 import * as S from './RollingPage.styled';
 import Card from '../../components/Card/Card';
+import useAsync from '../../hooks/useAsync';
+import { getMessageListRequest } from '../../apis/api';
 
 export default function RollingPage() {
+  const [messageList, setMessageList] = useState();
+  const { requestFunction: getMessageList } = useAsync(getMessageListRequest);
+
+  const getData = async () => {
+    const result = await getMessageList(5788);
+    if (!result) return;
+    const {
+      data: { results },
+    } = result;
+    setMessageList(results);
+  };
+
+  console.log(messageList);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <S.RollingPageLayout>
       <Inner>
         <S.CardContainer>
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
-          <li>
-            <Card />
-          </li>
+          {messageList?.map((item) => (
+            <li key={item.id}>
+              <Card
+                content={item.content}
+                profileImageURL={item.profileImageURL}
+                relationship={item.relationship}
+                sender={item.sender}
+                createdAt={item.createdAt}
+              />
+            </li>
+          ))}
         </S.CardContainer>
       </Inner>
     </S.RollingPageLayout>
