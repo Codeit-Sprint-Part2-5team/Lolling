31adac84a3c7344db642683b0ae94d905eae41c5
O-daeun <qzsz797979@gmail.com>
2024-04-12 11:04:45 +0900

    feat : RollingPage에 Card 컴포넌트 import하고 스타일 작업 중

--- src/pages/RollingPage/RollingPage.styled.js
+++ src/pages/RollingPage/RollingPage.styled.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export const RollingPageLayout = styled.div`
-  
-`;
+  background: var(--Orange-200, #FFE2AD);
+`; 
+
+
+export const CardContainer = styled.ul`
+  display: flex;
+  gap: 24px;
+  flex-wrap: wrap;
+`
