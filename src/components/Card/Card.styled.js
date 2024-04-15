c37fe3a7f48758aa80c00eb362e65402f525f6dc
O-daeun <qzsz797979@gmail.com>
2024-04-15 15:32:34 +0900

    design : Card 컴포넌트 cursor pointer로 변경

--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -9,6 +9,7 @@ export const CardLayout = styled.div`
   border-radius: 16px;
   box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
   background-color: var(--white, #fff);
+  cursor: pointer;
 
   ${({ $add }) =>
     $add &&
