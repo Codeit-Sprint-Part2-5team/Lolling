987f8d1ad1366303bef52a82a168bdf7cf163024
O-daeun <qzsz797979@gmail.com>
2024-04-15 16:05:47 +0900

    style : SenderProfile 컴포넌트 생성 및 Card 컴포넌트에 적용

---
+++ src/components/SenderProfile/SenderProflie.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const TopContainer = styled.div`
+  display: flex;
+  gap: 14px;
+  width: 100%;
+  padding-bottom: 15px;
+  border-bottom: 1px solid var(--gray-200, #eee);
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const NameContainer = styled.div`
+  display: flex;
+  gap: 6px;
+  font-size: 20px;
+  line-height: 24px;
+`;
