62cf7f49e54773720f839992cf4a357293dcec64
이보미 <qhal6550@naver.com>
2024-04-17 16:03:12 +0900

    design : 헤더 부분 태블릿 반응형

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -6,16 +6,40 @@ export const HeaderLayout = styled.header`
   align-items: center;
   height: 64px;
   border-bottom: 1px solid #ededed;
+
+  @media (max-width: 760px) {
+    display: flex;
+    width: 768px;
+    height: 64px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1px;
+    padding: 11px 24px;
+  }
 `;
 
 export const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media (max-width: 760px) {
+    display: flex;
+    align-items: center;
+    gap: 465px;
+  }
 `;
 
 export const LogoBox = styled.h1`
   width: fit-content;
+
+  @media (max-width: 760px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-items: center;
+  }
 `;
 
 export const LinkBox = styled(Link)`
@@ -25,4 +49,7 @@ export const LinkBox = styled(Link)`
   font-size: 19px;
   font-weight: 700;
   color: #4a494f;
+  text-align: center;
+  font-family: Poppins;
+  line-height: normal;
 `;
