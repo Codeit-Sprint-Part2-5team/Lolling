d55f4ef711b368c14d4c8acdd4e1209dcc547c43
eqypo9 <eqypo@naver.com>
2024-04-18 16:19:35 +0900

    fix: babel/plugin-proposal-private-property-in-object 설치

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Main';
-import ListPage from './pages/ListPage/ListPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <Main />
-    <ListPage />
   </div>
 );
