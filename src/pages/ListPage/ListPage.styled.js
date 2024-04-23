c271d3361e4a2b883f51b0b1aeb3719cc0639fef
O-daeun <qzsz797979@gmail.com>
2024-04-23 21:58:36 +0900

    design : Input focus됐을 때 border색 변경

--- src/pages/ListPage/ListPage.styled.js
+++ src/pages/ListPage/ListPage.styled.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import Inner from '../../components/Inner/Inner';
 
 export const CardInner = styled(Inner)`
