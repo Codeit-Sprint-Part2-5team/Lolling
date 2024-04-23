142db5feed03ab66b5dee9ebcc55474685778152
kidboi666 <kidboi666@gmail.com>
2024-04-23 15:02:32 +0900

    fix : 폰트 마진 줄이고 하단 패딩 추가

--- src/pages/MessagePage/MessagePage.styled.js
+++ src/pages/MessagePage/MessagePage.styled.js
@@ -8,7 +8,7 @@ export const PostPageLayout = styled.div`
 `;
 
 export const FormContainer = styled.form`
-  padding: 57px 0;
+  padding: 57px 0 130px;
   width: 720px;
 
   > div {
@@ -108,5 +108,4 @@ export const TextAreaContainer = styled.div`
 
 export const FontContainer = styled.div`
   ${FlexMargin}
-  margin-bottom: 206px;
 `;
